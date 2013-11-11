a1c3f0fd4ab5ba60ea4dd2f156f6c8ec760d4a5a
Jonathan Bennett <jonathan@jbennett.me>
2013-11-11 10:31:03 -0500

    Started using owl

--- src/files/scripts/script.js
+++ src/files/scripts/script.js
@@ -2,4 +2,13 @@
 	$('html')
 		.removeClass('no-js')
 		.addClass('js');
+
+
+	// owl carousel
+	$('.carousel').owlCarousel({
+		addClassActive: true,
+		navigation: true,
+		rewindNav: false,
+		singleItem: true
+	});
 })(jQuery);
